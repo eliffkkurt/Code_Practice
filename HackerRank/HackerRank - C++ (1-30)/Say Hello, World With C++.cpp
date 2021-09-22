1cb7cb80df773731e3fe4d0eb1ee10e1e04625b7
eliffkkurt <pegs.elif@gmail.com>
2021-09-22 11:25:53 +0300

    Add files via upload

---
+++ HackerRank/HackerRank - C++ (1-30)/Say Hello, World With C++.cpp
@@ -0,0 +1,12 @@
+
+#Say "Hello, World!" With C++
+#https://www.hackerrank.com/challenges/cpp-hello-world/problem
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("%s","Hello, World!");
+    return 0;
+}
